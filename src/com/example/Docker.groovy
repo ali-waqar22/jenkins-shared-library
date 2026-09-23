c41eb266ab1204acd7e119d48e8c30da28f8340b
Ali Waqar <aliwaqar.cys@gmail.com>
2026-09-23 21:41:20 +0500

    seperated docker steps and files

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -9,15 +9,17 @@ class Docker implements Serializable {
     }
 
 def buildDockerImage(String imageName) {
-        script.echo "building the docker image..."
-        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "docker build -t ${imageName} ."
-            
-            // Fix: Use double quotes, but escape the dollar signs so Groovy passes the literal string '$PASS' to the shell
-            script.sh "echo \$PASS | docker login -u \$USER --password-stdin"
-            
-            script.sh "docker push ${imageName}"
-        }
-    }
+    script.echo "building the docker image..."
+    script.sh "docker build -t ${imageName} ."
 
+}
+
+def dockerLogin() {
+    script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        script.sh "echo \$PASS | docker login -u \$USER --password-stdin"
+    }
+}
+
+def dockerPush(String imageName) {
+    script.sh "docker push ${imageName}"
 }
