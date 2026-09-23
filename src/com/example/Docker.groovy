78c43918e1e657f5e491d1e9ccfe790f95a90a45
Ali Waqar <aliwaqar.cys@gmail.com>
2026-09-23 21:30:23 +0500

    formatting issues resolved

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -8,12 +8,15 @@ class Docker implements Serializable {
         this.script = script
     }
 
-    def buildDockerImage(String imageName) {
+def buildDockerImage(String imageName) {
         script.echo "building the docker image..."
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            script.sh "docker build -t $imageName ."
-            script.sh "echo $PASS | docker login -u $USER --password-stdin"
-            script.sh "docker push $imageName"
+            script.sh "docker build -t ${imageName} ."
+            
+            // Fix: Use double quotes, but escape the dollar signs so Groovy passes the literal string '$PASS' to the shell
+            script.sh "echo \$PASS | docker login -u \$USER --password-stdin"
+            
+            script.sh "docker push ${imageName}"
         }
     }
 
